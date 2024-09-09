65976693ecfd46b53ea6082bd2e596a27aaeab43
朱一一 <zhuyiyi1990@outlook.com>
2024-09-09 10:31:27 +0800

    添加 leetcode problem 10

---
+++ src/main/java/com/github/zhuyiyi1990/algorithm/leetcode/problem0010/Solution.java
@@ -0,0 +1,9 @@
+package com.github.zhuyiyi1990.algorithm.leetcode.problem0010;
+
+public class Solution {
+
+    public boolean isMatch(String s, String p) {
+        return s.matches(p);
+    }
+
+}
