b951728a65fd130c152cab6697d3071b612730cd
朱一一 <zhuyiyi1990@outlook.com>
2026-02-03 14:42:23 +0800

    彩票

--- src/main/java/com/github/zhuyiyi1990/algorithm/lottery/MyMain.java
+++ src/main/java/com/github/zhuyiyi1990/algorithm/lottery/MyMain.java
@@ -1,18 +1,11 @@
 package com.github.zhuyiyi1990.algorithm.lottery;
 
-import java.security.SecureRandom;
-
 public class MyMain {
 
     public static void main(String[] args) {
         final int COUNT = 5;
         for (int i = 0; i < COUNT; i++) {
-            try {
-                Thread.sleep(new SecureRandom().nextInt(4001) + 1000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            String ticket = BestDoubleColorBall.generateBestTicket();
+            String ticket = DoubleColorBall.generateTicket();
             System.out.println(ticket);
         }
     }
