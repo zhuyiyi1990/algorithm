72a68ee875e8c8d36e8bde39e92fb1881c9bc6c4
朱一一 <zhuyiyi1990@outlook.com>
2026-04-01 13:23:20 +0800

    股票

--- src/main/java/com/github/zhuyiyi1990/algorithm/lottery/LotteryMain.java
+++ src/main/java/com/github/zhuyiyi1990/algorithm/lottery/LotteryMain.java
@@ -3,7 +3,7 @@ package com.github.zhuyiyi1990.algorithm.lottery;
 public class LotteryMain {
 
     public static void main(String[] args) {
-        final int COUNT = 5;
+        final int COUNT = 1;
         for (int i = 0; i < COUNT; i++) {
             String ticket = DoubleColorBall.generateTicket();
             System.out.println(ticket);
--- src/main/java/com/github/zhuyiyi1990/algorithm/stock/StockMain.java
+++ src/main/java/com/github/zhuyiyi1990/algorithm/stock/StockMain.java
@@ -6,13 +6,13 @@ import java.math.RoundingMode;
 public class StockMain {
 
     public static void main(String[] args) {
-        final BigDecimal costPrice = new BigDecimal("6.4280");
+        final BigDecimal costPrice = new BigDecimal("100.0000");
         final BigDecimal oneHundred = new BigDecimal("100");
-        for (int i = 1; i <= 10; i++) {
-            BigDecimal variation = costPrice.multiply(new BigDecimal(i)).divide(oneHundred, 4, RoundingMode.HALF_UP);
+        for (BigDecimal i = new BigDecimal("0.5"); i.compareTo(BigDecimal.TEN) <= 0; i = i.add(new BigDecimal("0.5"))) {
+            BigDecimal variation = costPrice.multiply(i).divide(oneHundred, 4, RoundingMode.HALF_UP);
             BigDecimal posValue = costPrice.add(variation);
             BigDecimal negValue = costPrice.subtract(variation);
-            System.out.format("%d%%   %.4f    -%d%%   %.4f%n", i, posValue, i, negValue);
+            System.out.format("%.1f%%   %.4f    -%.1f%%   %.4f%n", i, posValue, i, negValue);
         }
     }
 
